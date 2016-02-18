281e87a7bc0e9513642bf1efa271d1f78daf393a
Harry McKinney <HarryPMcKinney@Gmail.com>
2016-02-18 10:34:56 -0500

    Create other.php

---
+++ other.php
@@ -0,0 +1,22 @@
+<?php
+
+//alternative version of FizzBuzz 
+
+  for ($i = 1; $i <= 100; $i++)
+        {
+    if($i % 3 == 0 && $i % 5 ==0)
+        {
+    echo "FizzBuzz<br />";
+        }
+    elseif($i % 3 == 0){
+        echo "Fizz<br />";
+        }
+    elseif($i % 5 == 0){
+        echo "Buzz<br />";
+        }
+    else 
+        {
+        echo $i."<br />";
+        }
+      }
+?>
